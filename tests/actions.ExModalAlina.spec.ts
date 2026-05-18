e28c9523eb7d1d600fcc8cc9eb6af045c3fb846a
mirleanualina <mirleanu.alina@gmail.com>
2026-05-18 12:41:00 +0300

    Ex Modal window Tema p/u acasa 2

---
+++ tests/actions.ExModalAlina.spec.ts
@@ -0,0 +1,29 @@
+import test, { expect, Locator } from 'playwright/test';
+
+test.describe('Window page', () => {
+ test.beforeEach(async ({ page }) => {
+ await page.goto('http://localhost:4200/');
+ await page.getByText('Modal & Overlays').click();
+ await page.getByText('Window').click();
+ });
+ 
+ test('Open window form - action', async ({ page }) => {
+    const openwindowform = page.locator('nb-card', { hasText: 'Window form' })
+    await expect(openwindowform).toBeVisible();
+
+    const openwindowformButton = openwindowform.getByRole('button', { name: 'Open window form' });
+    const modalwindow = page.locator('nb-window', { hasText: 'Window'})
+
+    await test.step('click the Open window form button', async () => {
+    await expect(openwindowformButton).toBeVisible();
+    await openwindowformButton.click();
+    await expect(modalwindow).toBeVisible();
+    });
+
+    await test.step('click outside the modal', async () => {
+        await page.locator('body').click({ position:{x: 0, y: 0}});
+        await expect(modalwindow).not.toBeVisible();
+        });
+
+    });
+});
