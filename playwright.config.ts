489ab1f121f02a2270ebb9160349eed146989859
mirleanualina <mirleanu.alina@gmail.com>
2026-05-21 01:42:58 +0300

    Without legacy folder

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   timeout: 20000,
   testDir: './tests/playwright',
-  testIgnore: '/**legacy**/',
+  testIgnore: '**/legacy/**',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
